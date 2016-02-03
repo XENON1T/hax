9815357f0f302a4202fe199b42a2a12c6a12d3e2
Jelle Aalbers <jelleaalbers@gmail.com>
2016-02-03 15:28:00 +0100

    Fix waveform inspection

--- hax/waveforms.py
+++ hax/waveforms.py
@@ -19,7 +19,8 @@ class HTTPSClientAuthHandler(HTTPSHandler):
     def https_open(self, req):
         return self.do_open(self.getConnection, req)
 
-    def getConnection(self, host):
+    def getConnection(self, host, timeout):
+        # TODO: timout is not used, but is passed, can't delete it or error
         return http.client.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)
 
 
@@ -33,11 +34,11 @@ def inspect_events(events, focus='all'):
     if isinstance(events, pd.Series):
         events = pd.DataFrame([events])
 
-    for event in events:
+    for _, event in events.iterrows():
         # Construct the XED filename from the dataset number and event number
-        xed_filename = 'xe100_%06d_%04d_%06d.xed' % (1e4 * int(event.dataset_number / 1e4),
+        xed_filename = 'xe100_%06d_%04d_%06d.xed' % (int(event.dataset_number / 1e4),
                                                      event.dataset_number % 1e4,
-                                                     int(event.event_number / 1e6))
+                                                     int(event.event_number / 1e3))
         _inspect_event_xenon100(event.event_number, xed_filename, focus=focus)
 
 
@@ -50,7 +51,7 @@ def _inspect_event_xenon100(event_number, xed_filename, focus='all'):
     with tempfile.NamedTemporaryFile(delete=False) as f:
 
         if CONFIG['raw_data_access_mode'] == 'local':
-            filename = CONFIG['raw_data_location'].format(dataset_name=dataset_name, xed_filename=xed_filename)
+            filename = CONFIG['raw_data_local_path'].format(dataset_name=dataset_name, xed_filename=xed_filename)
 
         else:
             grid_url = CONFIG['raw_data_grid_url'].format(dataset_name=dataset_name, xed_filename=xed_filename)
@@ -69,10 +70,11 @@ def _inspect_event_xenon100(event_number, xed_filename, focus='all'):
             filename = f.name
 
         config_dict = {'pax': {'output': ['Plotting.PlotEventSummary' if focus == 'all' else 'Plotting.PeakViewer'],
-                                                    'encoder_plugin': None,
-                                                    'input_name':  filename,
-                                                    'events_to_process': [event_number],
-                                                    'output_name': 'SCREEN'}}
+                               'encoder_plugin': None,
+                               'pre_output': [],
+                               'input_name':  filename,
+                               'events_to_process': [event_number],
+                               'output_name': 'SCREEN'}}
         if focus != 'all':
             config_dict['Plotting.PeakViewer'] = {'starting_peak': focus}
 
