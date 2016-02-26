e51292ac5b55edbe9b53ca4cc85958058f262f2d
Christopher Tunnell <ctunnell@nikhef.nl>
2016-02-26 22:10:58 +0100

    Add time difference variables for e.g. Kr studies.

--- hax/treemakers/Basics.py
+++ hax/treemakers/Basics.py
@@ -93,3 +93,45 @@ class Basics(TreeMaker):
                                largest_coincidence=largest_area_of_type['coincidence']))
 
         return event_data
+
+class TimeDifferences(TreeMaker):
+    """Compute time differences between S1s and S2s
+
+    The convention is that these times are how much later the
+    second largest signal is from the main one.  For example,
+    this could be the time of the second largest S2 minus the
+    time of the largest S2.  Therefore, you'll have a positive
+    number if the second largest S2 came after the main S2.
+
+    Provides:
+     - dt_s1s: Time difference between two largest S1s
+     - dt_s2s: Time difference between two largest S2s
+
+    Notes:
+
+    * Positive times means the second largest signal is later.
+    * The code is intentionally not general for clarity.
+
+    """
+
+    extra_branches = ['peaks.hit_time_mean']
+    __version__ = '0.0.1'
+
+
+    def extract_data(self, event):
+        # If there are no interactions at all, we can't extract anything...
+        event_data = dict()
+
+        # Loop over S1s
+        if len(event.s1s) >= 2:
+            times = [event.peaks[i].hit_time_mean for i in event.s1s]
+            dt = times[1] - times[0]
+            event_data.update(dict(dt_s1s=dt))
+
+        # Loop over S2s
+        if len(event.s2s) >= 2:
+            times = [event.peaks[i].hit_time_mean for i in event.s2s]
+            dt = times[1] - times[0]
+            event_data.update(dict(dt_s2s=dt))
+
+        return event_data
