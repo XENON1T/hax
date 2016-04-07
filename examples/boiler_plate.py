3b7b6db99ecbf72439cec10e3411e06116729a02
Christopher Tunnell <ctunnell@nikhef.nl>
2016-04-07 15:10:39 +0200

    Update NR/ER code examples

--- examples/boiler_plate.py
+++ examples/boiler_plate.py
@@ -1,12 +1,19 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import matplotlib.mlab as mlab
 import matplotlib   # Needed for font size spec, color map transformation function bla bla
 import matplotlib.pyplot as plt
 matplotlib.rc('font', size=16)
 plt.rcParams['figure.figsize'] = (12.0, 10.0) # resize plots
 plt.set_cmap('viridis')
 
-import pandas as pd
-import numpy as np
 from matplotlib.colors import LogNorm
 from sklearn import mixture
 
 import hax
+
+def c(x):
+    """ get center"""
+    return 0.5*(x[1:]+x[:-1])
