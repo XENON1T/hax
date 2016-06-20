daaaf72b65a93ab8b4975d5bf6d93b728e57a16b
Christopher Tunnell <ctunnell@nikhef.nl>
2016-06-20 14:58:28 +0200

    Bump version: 0.3.3 → 0.3.4

--- hax/__init__.py
+++ hax/__init__.py
@@ -4,7 +4,7 @@ import inspect
 from configparser import ConfigParser
 import socket
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 # Stitch the package together
 # I am surprised this works (even if we do 'from hax' instead of 'from .')
