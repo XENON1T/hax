4ab7bdf9754597c8f725276ce7db972fb605a1fb
Christopher Tunnell <ctunnell@nikhef.nl>
2017-02-18 18:48:56 -0600

    Lichens work

--- hax/lichen.py
+++ hax/lichen.py
@@ -3,7 +3,22 @@
 Extend the Minitree produced DataFrames with derivative values.
 """
 class Lichen(object):
-    pass
+    def pre(self, df):
+        return df
+
+    def process(self, df):
+        df = self.pre(df)
+        df = self._process(df)
+        df = self.post(df)
+        return df
+
+    def _process(self, df):
+        raise NotImplementedError()
+
+    def post(self, df):
+        if 'temp' in df.columns:
+            return df.drop('temp', 1)
+        return df
 
 
 class RangeLichen(Lichen):
@@ -24,27 +39,23 @@ class RangeLichen(Lichen):
             raise NotImplemented()
         return self.allowed_range[0]
 
-    def pre(self, df):
-        return df
-
-    def process(self, df):
-        self.pre(df)
-        df = self._process(df)
-        self.post(df)
-
     def _process(self, df):
-        print(self.__class__.__name__)
         df[self.__class__.__name__] = (df[self.variable] > self.allowed_range[0]) & (df[self.variable] < self.allowed_range[1])
         return df
-
-    def post(self, df):
-        return df.drop('temp', 1)
 
 
 class ManyLichen(Lichen):
     lichen_list = []
 
     def process(self, df):
+        all_cuts_bool = None
         for lichen in self.lichen_list:
             df = lichen.process(df)
+
+            if all_cuts_bool is None:
+                all_cuts_bool = df[lichen.__class__.__name__]
+            else:
+                all_cuts_bool = all_cuts_bool & df[lichen.__class__.__name__]
+
+        df[self.__class__.__name__] = all_cuts_bool
         return df
--- hax/lichens/sciencerun0.py
+++ hax/lichens/sciencerun0.py
@@ -1,4 +1,6 @@
 import numpy as np
+from pax import units, configuration
+PAX_CONFIG = configuration.load_configuration('XENON1T')
 from hax.lichen import Lichen, RangeLichen, ManyLichen
 
 class AllCutsSR0(ManyLichen):
@@ -20,18 +22,13 @@ class S2Threshold(RangeLichen):
 
 
 class cS2Threshold(S2Threshold):
+    allowed_range = (100, np.inf)
     variable = 'cs2'
 
 
 class S1Threshold(RangeLichen):
     allowed_range = (3, 70)
     variable = 'cs1'
-
-
-class S1Threshold(RangeLichen):
-    allowed_range = (3, 70)
-    variable = 'cs1'
-
 
 class Fiducial(ManyLichen):
     def __init__(self):
@@ -44,6 +41,11 @@ class Fiducial(ManyLichen):
 
     class R(RangeLichen):
         variable = 'temp'
+
+        def pre(self, df):
+            df[self.variable] = np.sqrt(df['x']*df['x'] + df['y']*df['y'])
+            return df
+
         allowed_range = (0, 30)
 
 
@@ -59,11 +61,13 @@ class InteractionPeaksBiggest(ManyLichen):
 
     class S1(Lichen):
         def _process(self, df):
-            return df.s1 > df.largest_other_s1
+            df[self.__class__.__name__] = df.s1 > df.largest_other_s1
+            return df
 
     class S2(Lichen):
         def _process(self, df):
-            return df.s2 > df.largest_other_s2
+            df[self.__class__.__name__] = df.s2 > df.largest_other_s2
+            return df
 
 
 class DoubleScatterS2(Lichen):
@@ -74,37 +78,51 @@ class DoubleScatterS2(Lichen):
         return np.clip((2 * s2) ** 0.5, 70, float('inf'))
 
     def _process(self, df):
-        return df.largest_other_s2 < self.other_s2_bound(df.s2)
+        df[self.__class__.__name__] = df.largest_other_s2 < self.other_s2_bound(df.s2)
+        return df
 
 class Width(ManyLichen):
-    scale = 1.349
-    scaleratio = 1.349 / 3.32
-    sigma_0 = 0.23 / scaleratio
 
+    
     def __init__(self):
         self.lichen_list = [self.WidthHigh(),
                             self.WidthLow()]
 
-    class WidthHigh(RangeLichen):
-        variable = 'temp'
-        allowed_range = (-10, 0)
+    def s2_width_model(self, z):
+        v_drift = PAX_CONFIG['DEFAULT']['drift_velocity_liquid']
+        diffusion_constant = PAX_CONFIG['WaveformSimulator']['diffusion_constant_liquid']
+        w0 = 304 * units.ns
+        return np.sqrt(w0**2 - 3.6395 * diffusion_constant * z / v_drift**3)
 
-        def s2_width_up(self, dt):
-            return (0.8 + 0.5 / 500 * dt + np.sqrt(
-                (self.scale * self.sigma_0) ** 2 + self.coefficient2 * dt)) * self.scaleratio
 
+    def subpre(self, df):
+        # relative_s2_width
+        df['temp'] = df['s2_range_50p_area'] / Width.s2_width_model(self, df['z'])
+        return df
+
+    def relative_s2_width_bounds(s2, kind='high'):
+        x = 0.3 * np.log10(np.clip(s2, 150, 7000))
+        if kind == 'high':
+            return 2.3 - x
+        elif kind == 'low':
+            return -0.3 + x
+        raise ValueError("kind must be high or low")
+    
+
+    class WidthHigh(Lichen):
         def pre(self, df):
-            df['temp'] = df.s2_range_50p_area / 1000. - self.s2_width_up(df.drift_time / 1000.)
+            return Width.subpre(self, df)
+
+        def _process(self, df):
+            df[self.__class__.__name__] = (df.temp <= Width.relative_s2_width_bounds(df.s2,
+                                                                                     kind='high'))
             return df
 
     class WidthLow(RangeLichen):
-        variable = 'temp'
-        allowed_range = (0, 10)
-
-        def s2_width_low(self, dt):
-            return (-0.4 - 0.5 / 500 * dt + np.sqrt(
-                (self.scale * self.sigma_0) ** 2 + self.coefficient2 * dt)) * self.scaleratio
-
         def pre(self, df):
-            df['temp'] = df.s2_range_50p_area / 1000. - self.s2_width_low(df.drift_time / 1000.)
+            return Width.subpre(self, df)
+
+        def _process(self, df):
+            df[self.__class__.__name__] = (Width.relative_s2_width_bounds(df.s2,
+                                                                           kind='low') <= df.temp)
             return df
