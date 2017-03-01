a1b702c933ac0d1ff98a130ed3c2a9032c1c3420
Jelle Aalbers <jelleaalbers@gmail.com>
2017-03-01 16:29:08 -0600

    Try to fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ from unittest.mock import MagicMock
 sys.modules.update((mod_name, MagicMock()) for mod_name in ['numpy', 'numpy.ma', 'pandas',
                                                             'matplotlib', 'matplotlib.pyplot',
                                                             'ROOT', 'root_numpy',
-                                                            'numba',
+                                                            'numba', 'pax.utils', 'pax.InterpolatingMap',
                                                             'bson', 'dask', 'dask.dataframe', 'dask.multiprocessing',
                                                             'pax', 'pax.plugins.io.ROOTClass', 'pax.exceptions',
                                                             'pax.configuration', 'pax.datastructure',
