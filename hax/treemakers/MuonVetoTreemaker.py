11a1b9fb90cdefce6349ae84a1fceb644353d07d
Christopher Geis <geisch@uni-mainz.de>
2017-03-10 15:13:32 +0100

    Update MuonVetoTreemaker (#93)
    numpy wasn't imported

--- hax/treemakers/MuonVetoTreemaker.py
+++ hax/treemakers/MuonVetoTreemaker.py
@@ -1,8 +1,9 @@
 import hax
+import numpy as np
 
 class MuonVetoTreemaker(hax.minitrees.TreeMaker):
     """Computing properties of the Muon Veto Events
-    
+
     This TreeMaker will ignore all TPC related events which are stored as objects of PAX classes like 'peaks' or 'interactions'.
     It gets the information of the MV events from the array stored in events.all_hits as numpy dtype array similar to objects
     from the PAX 'Hit' class.
@@ -17,18 +18,19 @@ class MuonVetoTreemaker(hax.minitrees.TreeMaker):
     event entry (error before)
     V1.1.1 - 2017/02/15 - ChG: Added 'Hit_noise_sigma' for calculation
     of threshold for each hit
+    V2.0.0 - 2017/03/09 - Erik, ChG: Fixed import error in hax
     """
     
     extra_branches = ['*']  # Activate all of ROOT file
-    __version__ = '1.1.1'
+    __version__ = '2.0.0'
     uses_arrays = True
     
     def extract_data(self, event):  # This runs on each event
-        
+
         # Create default dictionary
         #values = defaultdict(list)
         values ={}
-        
+
         values['PMTs'] = []                       # PMT Channel which got hit in the event
         values['Hit_area'] = []                   # Area of signal in each PMT Channel [PE] really PE?
         values['Hit_height'] = []                 # Height of signal in each PMT Channel [PE/samples]
@@ -38,11 +40,11 @@ class MuonVetoTreemaker(hax.minitrees.TreeMaker):
         values['Hit_width'] = []                  # peak width of each PMT Channel [samples]
         values['Hit_noise_sigma'] = []            # Noise sigma of the pulse in which the hit was found [pe/samples]
         #values['NumPMTsHit'] = 0                  # Number of PMT Channels which got hit in the event
-        
+
         # Store start/stop time of event
         values['tStart'] = event.start_time       # Starttime of the event (Unixtime)
         values['tStop'] = event.stop_time         # Stoptime of the event (Unixtime)
-        
+
         # Some events do not have a single hits, whatever that means....if so, do nothing and jump to next event
         if not len(event.all_hits):
             values['PMTs'].append(np.nan)         # necessary to note fail if first event is empty (has no hits)
@@ -54,9 +56,9 @@ class MuonVetoTreemaker(hax.minitrees.TreeMaker):
             values['Hit_width'].append(np.nan)          #  |
             values['Hit_noise_sigma'].append(np.nan)    #  |
             return values
-        
+
         #values['NumPMTsHit'] = len(event.all_hits)
-        
+
         for i in range(len(event.all_hits)):
             values['PMTs'].append(event.all_hits[i].channel)
             values['Hit_area'].append(event.all_hits[i].area)
