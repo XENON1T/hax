9bb6694a1d8432a5035bc3e805371a9eea4b162d
Daniel Coderre <coderdj@gmail.com>
2017-06-09 16:33:57 -0500

    added automatic printing of tag version if there is a version (#108)

--- hax/runs.py
+++ hax/runs.py
@@ -326,6 +326,13 @@ def tags_selection(dsets=None, include=None, exclude=None, pattern_type='fnmatch
         dsets = dsets[_tags_match(dsets, include, pattern_type, ignore_underscore)]
     if exclude is not None:
         dsets = dsets[True ^ _tags_match(dsets, exclude, pattern_type, ignore_underscore)]
+    # Get version
+    for itag in include:
+        tags = collection.find_one({"tags": {"$elemMatch": {"name": itag, "version": {"$exists": True}}}})['tags']
+        if tags is None:
+            continue
+        tag = [i for i in tags if i['name'] == itag][0]
+        print("Tag '"+ str(itag) + "' version: " + str(tag['version'] + " compiled on UTC " + str(tag['date'])))
 
     return dsets
 
