004c3db61d5cf117f7c8363a3d38746dbebdad70
Patrick de Perio <pdeperio@users.noreply.github.com>
2017-06-29 00:39:41 -0400

    re-add numba for ROOT bug (#115)

--- hax/__init__.py
+++ hax/__init__.py
@@ -3,6 +3,7 @@ import os
 import inspect
 from configparser import ConfigParser
 import socket
+import numba  # flake8: noqa: F401
 from . import misc, minitrees, paxroot, pmt_plot, raw_data, runs, utils, treemakers, data_extractor, slow_control, \
     trigger_data, ipython, recorrect  # flake8: noqa: F401
 __version__ = '1.4.4'
