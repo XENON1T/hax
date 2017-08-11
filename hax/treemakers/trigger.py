22170792a4c920fdda171b6e0e91cc2d1f363bf0
Mohamed Lotfi Benabderrahmane <mlotfiben@users.noreply.github.com>
2017-08-11 19:05:08 +0100

    Proximity class modification (#125)
    * Proximity class modification
    
    I have add modification to the Proximity inline with what is needed to calculate the nearest MV trigger to the TPC trigger.
    In these changes I have included two new functions and in addition a time correction to take into consideration the reference of the TPC trigger time.
    The two functions added do the following:
    1- In the MV data we have a new stream that is the MV synchronisation signal that is read by the TPC as a physical trigger sent by the MV. Which is in reality not true, because this signal was included just to keep the rollover of the digitiser working properly due to the low MV trigger rate and to be able to synchronise the two detectors. This signal has to be taken away from the acqm_monitor files to be able to have only the real physical MV triggers.
    2- Also in the MV data stream sent to the TPC, afterpulses are still present and are taken as a physical triggers to calculate the coincidence between the TPC and the MV. These afterpulses are cleaned away.
    
    * Update trigger.py
    
    * Update trigger.py
    
    * Update trigger.py
    
    * Update trigger.py
    
    * Update trigger.py
    
    * Update trigger.py
    
    * Update trigger.py
    
    * Update trigger.py
    
    * Update trigger.py

--- hax/treemakers/trigger.py
+++ hax/treemakers/trigger.py
@@ -5,7 +5,6 @@ import hax
 from hax.minitrees import TreeMaker
 from hax.trigger_data import get_aqm_pulses
 
-
 class LargestTriggeringSignal(TreeMaker):
     """Information on the largest trigger signal with the trigger flag set in the event
 
@@ -25,26 +24,25 @@ class LargestTriggeringSignal(TreeMaker):
         ts = tss[int(np.argmax([t.n_pulses for t in tss]))]
         return {"trigger_" + k: getattr(ts, k) for k in [a[0] for a in TriggerSignal.get_fields_data(TriggerSignal())]}
 
-
 class Proximity(hax.minitrees.TreeMaker):
     """Information on the proximity of other events and acquisition monitor signals (e.g. busy and muon veto trigger)
-    Provides:
-     - previous_x: Time (in ns) between the time center of the event and the previous x (see below for various x).
-       This also considers any x inside the event.
-     - next_x: same, for time to next x
-     - nearest_x: Time to the nearest x. NB: If the nearest x is in the past, this time is negative!
-
-    x denotes the object of interest, and could be either:
-     - muon_veto_trigger.
-     - busy_x: a busy-on or busy-off signal
-     - hev_x: a high-energy veto on or -off signal
-     - event: any event (excluding itself :-)
-     - 1e5_pe_event: any event with total peak area > 1e5 pe (excluding itself)
-     - 3e5_pe_event: "" 3e5 pe "
-     - 1e6_pe_event: "" 1e6 pe "
-     - s2_area: Area of main s2 in event
+        Provides:
+         - previous_x: Time (in ns) between the time center of the event and the previous x (see below for various x).
+           This also considers any x inside the event.
+         - next_x: same, for time to next x
+         - nearest_x: Time to the nearest x. NB: If the nearest x is in the past, this time is negative!
+            x denotes the object of interest, and could be either:
+         - muon_veto_trigger.
+         - busy_x: a busy-on or busy-off signal
+         - hev_x: a high-energy veto on or -off signal
+         - event: any event (excluding itself :-)
+         - 1e5_pe_event: any event with total peak area > 1e5 pe (excluding itself)
+         - 3e5_pe_event: "" 3e5 pe "
+         - 1e6_pe_event: "" 1e6 pe "
+         - s2_area: Area of main s2 in event
+         - All the information about the muon_veto_trigger are calculated with respect to the TPC trigger and not to the middle of the event.
     """
-    __version__ = '0.0.12'
+    __version__ = '0.1.0'
     pax_version_independent = False          # Now that we include S2 area it's not
 
     aqm_labels = [
@@ -55,10 +53,45 @@ class Proximity(hax.minitrees.TreeMaker):
         'hev_off',
         'busy',
         'hev']
+    def bad_mv_triggers(self, aqm_pulses, min_time =500):
+        """
+        get me the indices of the sync signal in the MV_trigger sent to the TPC
+        """
+        indices_mv = list()
 
+        for tsync in aqm_pulses['mv_sync']:
+            #This dict will have the info of the unwanted MV_triggers sent to TPC
+            #maybe there is a better way to save those!!!
+            list_times = dict()
+            for j,tmvt in enumerate(aqm_pulses['muon_veto_trigger']):
+                list_times[j] = np.abs(tsync - tmvt)
+            #these are the indices of the MV_trigger that needs to be taken away
+            indices_mv.extend([key for key, value in list_times.items() if value < min_time])
+        return indices_mv
+
+    
+    def select_physical_pulses(self, aqm_pulses,ap_time = 20000):
+        """
+        get rid of afterpulses from the MV data of course!!!!!
+        """
+        # lets get rid of the trigger that comes from the mv_sync signal
+        mask = np.ones(aqm_pulses["muon_veto_trigger"].shape, dtype=bool)
+        mask[self.bad_mv_triggers(aqm_pulses)] = False
+        list_indicies = list()
+        for l, times  in enumerate(np.diff(aqm_pulses["muon_veto_trigger"][mask])):
+            if times < ap_time: # get rid of pulses that comes within 20mu sec.
+                list_indicies.append(l+1)
+        
+        aqm_pulses["muon_veto_trigger"] = np.delete(aqm_pulses["muon_veto_trigger"][mask], list_indicies)
+              
+        return aqm_pulses
+                               
+                               
+                               
     def get_data(self, dataset, event_list=None):
-        aqm_pulses = get_aqm_pulses(dataset)
-
+        
+        aqm_pulses = self.select_physical_pulses(get_aqm_pulses(dataset))
+        
         # Load the fundamentals and totalproperties minitree
         # Yes, minitrees loading other minitrees, the fun has begun :-)
         event_data = hax.minitrees.load_single_dataset(
@@ -83,10 +116,16 @@ class Proximity(hax.minitrees.TreeMaker):
         # Again, integer division is not optional here!
         t = (event.start_time + event.stop_time) // 2
         result = dict()
-
+        
         for label, x in self.search_these:
+            # we want to use for the MV the time with respect to the trigger in the TPC
+            if label == "muon_veto_trigger":
+                t_corr = np.int64(event.stop_time - event.start_time) // 2 - np.int64(10**6)
+            else:
+                t_corr = np.int64(0)
+                
             prev = 'previous_%s' % label
-            nxt = 'next_%s' % label
+            nxt  = 'next_%s' % label
 
             # Find the first object (at or) after t
             if label == 'event':
@@ -128,10 +167,11 @@ class Proximity(hax.minitrees.TreeMaker):
             # is in the past.
             tnr = 'nearest_' + label
             if result[nxt] > result[prev]:
-                result[tnr] = - result[prev]
+                result[tnr] = - result[prev] + t_corr
             else:
-                result[tnr] = result[nxt]
-
+                result[tnr] = result[nxt] + t_corr
+                
+                        
         # Need special logic for nearest s2 area
         if result['nearest_event'] == result['previous_event']:
             result['nearest_s2_area'] = result['previous_s2_area']
@@ -139,7 +179,6 @@ class Proximity(hax.minitrees.TreeMaker):
             result['nearest_s2_area'] = result['next_s2_area']
 
         return result
-
 
 class TailCut(hax.minitrees.TreeMaker):
 
