284a3005def784fea6b51c4a3bb2a7477a5280bf
Daniel Coderre <daniel.coderre@physik.uni-freiburg.de>
2017-12-25 19:16:20 +0100

    New reconstruction and dependent variables (#174)
    * Added posrec treemaker
    
    * Updates to posrec treemaker
    
    * Bug fixes
    
    * placeholder for tensorflow based pos_reconstruction
    
    * minor style issue
    
    * working version
    
    * lint fixes
    
    * Update posrec.py (#177)
    
    * Properly declare size of array
    
    * ignor bad pattern with hits
    
    * Added s1patternfit for bottom. Also split files into ini options
    
    * Fix S1 bottom pattern, docs, and lint (#180)
    
    * Fix S1 bottom pattern, docs, and lint
    
    * Fix oops
    
    * Increment version
    
    * Fixed some bugs and made it run
    
    * Update posrec.py
    
    add s1 area fraction near injection points near Rn220 source for anomalous leakage studies. Details can be seen in the following note.
    https://xe1t-wiki.lngs.infn.it/doku.php?id=xenon:xenon1t:analysis:sciencerun1:anomalous_background#can_we_remove_leakage_events_by_tightening_some_cuts
    
    * Update posrec.py
    
    * Update posrec.py
    
    * Update posrec.py
    
    * Address some of Chris' comments and lint fixes
    
    * Update NN files
    according to https://github.com/XENON1T/pax/pull/650
    
    * NN file switching depending on SR (#181)
    
    * Add capability to switch NN files for SR
    and more metadata and logging and lint fixes
    
    * More comments
    
    * Fix two bugs in new S1 AFT and pattern likelihood calculations (#183)
    
    * Fix bugs in event indexing and PMT selection
    for S1 likelihood calculations
    
    * Increment version
    
    * Fix version oops
    
    0.2 is actually less than 0.16
    
    * Add SR dependent bad PMT list (#184)
    
    * Pull corrections logic out of minitree maker (#185)
    
    * Added new corrections handler class
    
    * Made it work
    
    * Made posrec treemaker work with new class too
    
    * Revert version bump
    
    * Oops. Moved instead of git moved.
    
    * Implement in CorrectedDoubleS1Scatter and lint fixes
    
    * Add 3D FDC for TFNN in SR1
    
    See details here:
    https://github.com/XENON1T/pax/pull/655
    
    * Add 3D FDC for TFNN in SR1
    
    See details here:
    https://github.com/XENON1T/pax/pull/655
    
    * Update version number
    
    Forgot again...
    
    * fix bug
    
    Name should be 'event_data' instead of 'result'...
    
    * fix bugs
    
    * fix bugs 2
    
    * Update posrec.py
    
    * Update posrec.py
    
    adding additional s1 width variables for AC events rejection
    
    * Update posrec.py
    
    * Add 3D FDC for TFNN in SR0
    
    * More S1 AFT variables (#188)
    
    * Store additional AFT var using area instead of hits
    and AFT probability from map
    
    * Fix variable name
    
    * Store binomial probability instead of aft
    
    * Remove S1 width variables
    
    Moved to Extended minitree in #190
    
    * sort the tensorflow files name
    
    * Update posrec.py
    
    * Make bad PMT list read-in work

---
+++ hax/treemakers/posrec.py
@@ -0,0 +1,294 @@
+import hax
+import numpy as np
+import json
+from hax.minitrees import TreeMaker
+from pax.PatternFitter import PatternFitter
+from pax.configuration import load_configuration
+from pax import utils
+from pax import exceptions
+from pax.plugins.interaction_processing.S1AreaFractionTopProbability import binom_test, binom_pmf
+from keras.models import model_from_json
+from hax.corrections_handler import CorrectionsHandler
+
+
+class PositionReconstruction(TreeMaker):
+    """Stores position-reconstruction-related variables.
+
+    Provides:
+       - s1_pattern_fit_hax: S1 pattern likelihood computed with corrected
+                             position and areas
+       - s1_pattern_fit_hits_hax: S1 pattern likelihood computed with corrected
+                                  position and hits
+       - s1_pattern_fit_bottom_hax: S1 pattern likelihood computed with corrected
+                                    position and bottom array area
+       - s1_pattern_fit_bottom_hits_hax: S1 pattern likelihood computed with corrected
+                                         position and bottom array hits
+
+       - s1_area_fraction_top_probability_hax: S1 AFT p-value computed with corrected position
+       - s1_area_fraction_top_probability_nothresh: computed using area below S1=10 (instead of hits)
+       - s1_area_fraction_top_binomial: Binomial probability for given S1 AFT
+       - s1_area_fraction_top_binomial_nothresh: Same except using area below S1=10
+
+       - x_observed_nn_tf: TensorFlow NN reconstructed x position
+       - y_observed_nn_tf: TensorFlow NN reconstructed y position
+       - r_observed_nn_tf: TensorFlow NN reconstructed r position
+
+       - r_3d_nn_tf: the corrected interaction r coordinate (data-driven 3d fdc)
+       - x_3d_nn_tf: the corrected interaction x coordinate (data-driven 3d fdc)
+       - y_3d_nn_tf: the corrected interaction y coordinate (data-driven 3d fdc)
+       - z_3d_nn_tf: the corrected interaction z coordinate (data-driven 3d fdc)
+       - r_correction_3d_nn_tf: r_3d_nn_tf - r_observed_nn_tf
+       - z_correction_3d_nn_tf: z_3d_nn_tf - z_observed
+
+       - s1_area_upper_injection_fraction: s1 area fraction near Rn220 injection points (near PMT 131)
+       - s1_area_lower_injection_fraction: s1 area fraction near Rn220 injection points (near PMT 243)
+    """
+    __version__ = '0.19'
+    extra_branches = ['peaks.area_per_channel[260]',
+                      'peaks.hits_per_channel[260]',
+                      'peaks.n_saturated_per_channel[260]',
+                      'interactions.x', 'interactions.y', 'interactions.z']
+
+    def __init__(self):
+
+        hax.minitrees.TreeMaker.__init__(self)
+        self.extra_metadata = hax.config['corrections_definitions']
+        self.corrections_handler = CorrectionsHandler()
+
+        # We need to pull some stuff from the pax config
+        self.pax_config = load_configuration("XENON1T")
+        self.tpc_channels = self.pax_config['DEFAULT']['channels_in_detector']['tpc']
+        self.confused_s1_channels = []
+        self.statistic = (
+            self.pax_config['BuildInteractions.BasicInteractionProperties']['s1_pattern_statistic']
+        )
+        qes = np.array(self.pax_config['DEFAULT']['quantum_efficiencies'])
+
+        self.pattern_fitter = PatternFitter(
+            filename=utils.data_file_name(self.pax_config['WaveformSimulator']['s1_patterns_file']),
+            zoom_factor=self.pax_config['WaveformSimulator'].get('s1_patterns_zoom_factor', 1),
+            adjust_to_qe=qes[self.tpc_channels],
+            default_errors=(self.pax_config['DEFAULT']['relative_qe_error'] +
+                            self.pax_config['DEFAULT']['relative_gain_error'])
+        )
+
+        # Threshold for s1_aft probability calculation
+        self.low_pe_threshold = 10
+
+        self.ntop_pmts = len(self.pax_config['DEFAULT']['channels_top'])
+
+        # Declare nn stuff
+        self.tfnn_weights = None
+        self.tfnn_model = None
+        self.loaded_nn = None
+
+    def load_nn(self):
+        """For loading NN files"""
+
+        # If we already loaded it up then skip
+        if ((self.tfnn_weights == self.corrections_handler.get_misc_correction(
+                "tfnn_weights", self.run_number)) and
+            (self.tfnn_model == self.corrections_handler.get_misc_correction(
+                "tfnn_model", self.run_number))):
+            return
+
+        self.tfnn_weights = self.corrections_handler.get_misc_correction(
+            "tfnn_weights", self.run_number)
+        self.tfnn_model = self.corrections_handler.get_misc_correction(
+            "tfnn_model", self.run_number)
+
+        json_file_nn = open(utils.data_file_name(self.tfnn_model), 'r')
+        loaded_model_json = json_file_nn.read()
+        self.loaded_nn = model_from_json(loaded_model_json)
+        json_file_nn.close()
+
+        # Get bad PMT List in JSON file:
+        json_file_nn = open(utils.data_file_name(self.tfnn_model), 'r')
+        loaded_model_json_dict = json.load(json_file_nn)
+        self.list_bad_pmts = loaded_model_json_dict['badPMTList']
+        json_file_nn.close()
+
+        weights_file = utils.data_file_name(self.tfnn_weights)
+        self.loaded_nn.load_weights(weights_file)
+        self.loaded_nn.compile(loss='mean_squared_error', optimizer='adam')
+
+    def get_data(self, dataset, event_list=None):
+        # If we do switch to new NN later get rid of this stuff and directly use those positions!
+        data, _ = hax.minitrees.load_single_dataset(dataset, ['Corrections', 'Fundamentals'])
+        self.x = data.x_3d_nn.values
+        self.y = data.y_3d_nn.values
+        self.z = data.z_3d_nn.values
+
+        self.indices = list(data.event_number.values)
+
+        return hax.minitrees.TreeMaker.get_data(self, dataset, event_list)
+
+    def extract_data(self, event):
+
+        event_data = {
+            "s1_pattern_fit_hax": None,
+            "s1_pattern_fit_hits_hax": None,
+            "s1_pattern_fit_bottom_hax": None,
+            "s1_pattern_fit_bottom_hits_hax": None,
+            "s1_area_fraction_top_probability_hax": None,
+            "s1_area_fraction_top_probability_nothresh": None,
+            "s1_area_fraction_top_binomial": None,
+            "s1_area_fraction_top_binomial_nothres": None,
+            "x_observed_nn_tf": None,
+            "y_observed_nn_tf": None,
+            "r_observed_nn_tf": None,
+            "x_3d_nn_tf": None,
+            "y_3d_nn_tf": None,
+            "r_3d_nn_tf": None,
+            "z_3d_nn_tf": None,
+            "r_correction_3d_nn_tf": None,
+            "z_correction_3d_nn_tf": None,
+            "s1_area_upper_injection_fraction": None,
+            "s1_area_lower_injection_fraction": None,
+        }
+
+        # We first need the positions. This minitree is only valid when loading
+        # Corrections since you need that to get the corrected positions
+        if not len(event.interactions):
+            return event_data
+
+        # Check that correct NN is loaded and change if not
+        self.load_nn()
+
+        event_num = event.event_number
+
+        try:
+            event_index = self.indices.index(event_num)
+        except Exception:
+            return event_data
+
+        interaction = event.interactions[0]
+        s1 = event.peaks[interaction.s1]
+        s2 = event.peaks[interaction.s2]
+
+        # Position reconstruction based on NN from TensorFlow
+        s2apc = np.array(list(s2.area_per_channel))
+        s2apc_clean = []
+
+        for ipmt, s2_t in enumerate(s2apc):
+            if ipmt not in self.list_bad_pmts and ipmt < self.ntop_pmts:
+                s2apc_clean.append(s2_t)
+
+        s2apc_clean = np.asarray(s2apc_clean)
+        s2apc_clean_norm = s2apc_clean / s2apc_clean.sum()
+        s2apc_clean_norm = s2apc_clean_norm.reshape(1, len(s2apc_clean_norm))
+
+        predicted_xy_tensorflow = self.loaded_nn.predict(s2apc_clean_norm)
+        event_data['x_observed_nn_tf'] = predicted_xy_tensorflow[0, 0] / 10.
+        event_data['y_observed_nn_tf'] = predicted_xy_tensorflow[0, 1] / 10.
+        event_data['r_observed_nn_tf'] =\
+            np.sqrt(event_data['x_observed_nn_tf']**2 + event_data['y_observed_nn_tf']**2)
+
+        # 3D FDC
+        algo = 'nn_tf'
+        z_observed = interaction.z - interaction.z_correction
+        cvals = [event_data['x_observed_' + algo], event_data['y_observed_' + algo], z_observed]
+        event_data['r_correction_3d_' + algo] = self.corrections_handler.get_correction_from_map(
+            "fdc_3d_tfnn", self.run_number, cvals)
+
+        event_data['r_3d_' + algo] = event_data['r_observed_' + algo] + event_data['r_correction_3d_' + algo]
+        event_data['x_3d_' + algo] =\
+            event_data['x_observed_' + algo] * (event_data['r_3d_' + algo] / event_data['r_observed_' + algo])
+        event_data['y_3d_' + algo] =\
+            event_data['y_observed_' + algo] * (event_data['r_3d_' + algo] / event_data['r_observed_' + algo])
+
+        if abs(z_observed) > abs(event_data['r_correction_3d_' + algo]):
+            event_data['z_3d_' + algo] = -np.sqrt(z_observed ** 2 -
+                                                  event_data['r_correction_3d_' + algo] ** 2)
+        else:
+            event_data['z_3d_' + algo] = z_observed
+
+        event_data['z_correction_3d_' + algo] = event_data['z_3d_' + algo] - z_observed
+
+        # s1 area fraction near injection points for Rn220 source
+        area_upper_injection = (s1.area_per_channel[131] + s1.area_per_channel[138] +
+                                s1.area_per_channel[146] + s1.area_per_channel[147])
+        area_lower_injection = (s1.area_per_channel[236] + s1.area_per_channel[237] +
+                                s1.area_per_channel[243])
+
+        event_data['s1_area_upper_injection_fraction'] = area_upper_injection / s1.area
+        event_data['s1_area_lower_injection_fraction'] = area_lower_injection / s1.area
+
+        # Want S1 AreaFractionTop Probability
+        if s1.area < self.low_pe_threshold:
+            s1_frac = s1.area / self.low_pe_threshold
+            hits_top = s1.n_hits * s1.hits_fraction_top
+            s1_top = s1.area * s1.area_fraction_top
+            size_top = hits_top * (1. - s1_frac) + s1_top * s1_frac
+            size_tot = s1.n_hits * (1. - s1_frac) + s1.area * s1_frac
+        else:
+            size_top = s1.area * s1.area_fraction_top
+            size_tot = s1.area
+
+        aft = self.corrections_handler.get_correction_from_map(
+            "s1_aft_map", self.run_number, [self.x[event_index],
+                                            self.y[event_index], self.z[event_index]])
+
+        event_data['s1_area_fraction_top_binomial'] = binom_pmf(size_top, size_tot, aft)
+        event_data['s1_area_fraction_top_probability_hax'] = binom_test(size_top, size_tot, aft)
+
+        # Use area below S1=10 instead of hits
+        if s1.area < self.low_pe_threshold:
+            size_top = s1.area * s1.area_fraction_top
+            size_tot = s1.area
+
+            event_data['s1_area_fraction_top_probability_nothresh'] = binom_test(size_top, size_tot, aft)
+            event_data['s1_area_fraction_top_binomial_nothresh'] = binom_pmf(size_top, size_tot, aft)
+
+        else:
+            event_data['s1_area_fraction_top_probability_nothresh'] = event_data['s1_area_fraction_top_probability_hax']
+            event_data['s1_area_fraction_top_binomial_nothresh'] = event_data['s1_area_fraction_top_binomial']
+
+        # Now do s1_pattern_fit
+        apc = np.array(list(s1.area_per_channel))
+        hpc = np.array(list(s1.hits_per_channel))
+
+        # Get saturated channels
+        confused_s1_channels = []
+        for a, c in enumerate(s1.n_saturated_per_channel):
+            if c > 0:
+                confused_s1_channels.append(a)
+
+        try:
+
+            # Create PMT array of booleans for use in likelihood calculation
+            is_pmt_in = np.ones(len(self.tpc_channels), dtype=bool)  # Default True
+            is_pmt_in[confused_s1_channels] = False  # Ignore saturated channels
+
+            event_data['s1_pattern_fit_hax'] = self.pattern_fitter.compute_gof(
+                (self.x[event_index], self.y[event_index], self.z[event_index]),
+                apc[self.tpc_channels],
+                pmt_selection=is_pmt_in,
+                statistic=self.statistic)
+
+            event_data['s1_pattern_fit_hits_hax'] = self.pattern_fitter.compute_gof(
+                (self.x[event_index], self.y[event_index], self.z[event_index]),
+                hpc[self.tpc_channels],
+                pmt_selection=is_pmt_in,
+                statistic=self.statistic)
+
+            # Switch to bottom PMTs only
+            is_pmt_in[self.pax_config['DEFAULT']['channels_top']] = False
+
+            event_data['s1_pattern_fit_bottom_hax'] = self.pattern_fitter.compute_gof(
+                (self.x[event_index], self.y[event_index], self.z[event_index]),
+                apc[self.tpc_channels],
+                pmt_selection=is_pmt_in,
+                statistic=self.statistic)
+
+            event_data['s1_pattern_fit_bottom_hits_hax'] = self.pattern_fitter.compute_gof(
+                (self.x[event_index], self.y[event_index], self.z[event_index]),
+                hpc[self.tpc_channels],
+                pmt_selection=is_pmt_in,
+                statistic=self.statistic)
+
+        except exceptions.CoordinateOutOfRangeException as _:
+            # pax does this too. happens when event out of TPC (usually z)
+            return event_data
+
+        return event_data
