83d39693b255d854e2a31485c140aa5e9613a26a
pdeperio <pdeperio@gmail.com>
2018-01-18 12:00:38 -0600

    Bump version: 2.3.2 → 2.3.3

--- hax/__init__.py
+++ hax/__init__.py
@@ -6,7 +6,7 @@ import socket
 import numba  # flake8: noqa: F401
 from . import misc, minitrees, paxroot, pmt_plot, raw_data, runs, utils, treemakers, data_extractor, slow_control, \
     trigger_data, ipython, recorrect  # flake8: noqa: F401
-__version__ = '2.3.2'
+__version__ = '2.3.3'
 
 
 # Stitch the package together
