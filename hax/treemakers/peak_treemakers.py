09452b402deb206f445ceecb2ffc806acad6615e
Katrina Miller <krm29@uchicago.edu>
2018-01-29 16:34:20 -0600

    SE treemaker (#203)
    * SE treemaker
    
    This is the treemaker to produce the minitrees necessary to create the single electron gain stability plot and the average single electron waveform shape.
    
    * Update se_treemaker.py
    
    * Automatic Codacy error fix with autopep8
    
    * Automatic Codacy error fix with autopep8
    
    * Documentation update and slight refactor
    
    Responding to pull request comments
    
    * Lint fixes
    
    * Update single_electrons.py
    
    * Update single_electrons.py
    
    * Move SE code into existing peak treemaker file
    
    * Remove as put into peak_treemaker

--- hax/treemakers/peak_treemakers.py
+++ hax/treemakers/peak_treemakers.py
@@ -1,6 +1,12 @@
+"""Tree makers for studying peaks on their own
+
+Treemakers used for analyses such as the single-electron shape in time
+and stability.
+"""
+import hax
 from hax.minitrees import MultipleRowExtractor
 import numpy as np
-import hax
+from pax import units
 
 
 class PeakExtractor(MultipleRowExtractor):
@@ -92,3 +98,79 @@ class PeakExtractor(MultipleRowExtractor):
         else:
             # If event does not pass cut return empty list
             return []
+
+
+class IsolatedPeaks(MultipleRowExtractor):  #pylint: disable=unused-variable
+    """Returns one row per peak isolated in time
+
+    Specifically returns properties of each individual peak.
+    """
+    __version__ = '0.1.2'
+    extra_branches = ['peaks.left', 'peaks.right', 'peaks.n_hits',
+                      'peaks.n_contributing_channels',
+                      'peaks.reconstructed_positions*']
+
+    nhits_bounds = (0, float('inf'))
+    width_bounds = (0, float('inf'))
+
+    def extract_data(self, event):
+        results = []
+        for peak, time_to_nearest_peak in self.yield_peak(event,
+                                                          self.nhits_bounds,
+                                                           self.width_bounds):
+            result = dict({x: getattr(peak, x)
+                           for x in ['area', 'area_fraction_top', 'n_hits']})
+            result['time_to_nearest_peak'] = time_to_nearest_peak
+            result['range_50p_area'] = peak.range_area_decile[5]
+            result['n_contributing_channels'] = peak.n_contributing_channels
+            for rp in peak.reconstructed_positions:
+                if rp.algorithm == 'PosRecTopPatternFit':
+                    result['x'] = rp.x
+                    result['y'] = rp.y
+                    result['xy_gof'] = rp.goodness_of_fit
+                    break
+            results.append(result)
+
+        return results
+
+    @staticmethod
+    def yield_peak(event, nhits_bounds, width_bounds):
+        """Extracts a row per peak
+        
+        The peak type can be single electron and have some selection.  This is
+        a generator, and yields (peak, time_to_nearest).
+        """
+        # Get all non-lone-hit peaks in the TPC
+        peaks = [p for p in event.peaks if p.detector == 'tpc' and not p.type == 'lone_hit']
+        peaks = sorted(peaks, key=lambda p: p.left)
+
+        if not len(peaks):
+            return []
+
+        # For each of these, find the smallest gap
+        lefts, rights = np.array([(p.left, p.right) for p in peaks]).T * 10 * units.ns
+        gap_on_left = np.concatenate(([0], lefts[1:] - rights[:-1]))
+        gap_on_right = np.concatenate((lefts[1:] - rights[:-1], [0]))
+        smallest_gap = np.clip(gap_on_left, 0, gap_on_right)
+
+        # ses = []
+        # time_to_nearest = []
+        for i, peak in enumerate(peaks):
+            time_to_nearest_peak = smallest_gap[i]
+            if time_to_nearest_peak < 10 * units.us:
+                continue
+            if not (nhits_bounds[0] <= peak.n_hits < nhits_bounds[1]):
+                continue
+            width = peak.range_area_decile[5]
+            if not (width_bounds[0] <= width < width_bounds[1]):
+                continue
+            yield peak, time_to_nearest_peak
+
+
+class SingleElectrons(IsolatedPeaks):
+    """Specifically select single electrons
+    
+    Create one row per single electron, which has an area requirement.
+    """
+    nhits_bounds = (15, 26.01)    # 26 is in
+    width_bounds = (50, 450)
